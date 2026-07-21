d108f351af8c3d09bc25c8a8740d6d6e896a1b86
hssHigor <higorhss@outlook.com>
2026-07-21 20:01:51 -0300

    refactor: ajusta configuracao da API

--- ControleGastos.Api/Program.cs
+++ ControleGastos.Api/Program.cs
@@ -33,6 +33,11 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 app.UseCors("frontend");
 
 if (app.Environment.IsDevelopment())
