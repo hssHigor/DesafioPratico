370f4b0ea59881deab89e2f197f286a8ed68bbc0
hssHigor <higorhss@outlook.com>
2026-07-21 14:32:10 -0300

    feat: adiciona navegacao entre paginas

--- ControleGastos.Web/src/App.tsx
+++ ControleGastos.Web/src/App.tsx
@@ -1,22 +1,46 @@
+import { useState } from "react";
+
 import Pessoas from "./pages/Pessoas/Pessoas";
 import { Transacoes } from "./pages/Transacoes/Transacoes";
 import { Relatorio } from "./pages/Relatorio/Relatorio";
 
 
 function App() {
-  return (
-    <>
-      <Pessoas />
 
-      <hr />
+    const [pagina, setPagina] = useState("pessoas");
 
-      <Transacoes />
 
-      <hr />
+    return (
+        <>
+            <h1>Controle de Gastos</h1>
 
-      <Relatorio />
-    </>
-  )
+
+            <nav>
+                <button onClick={() => setPagina("pessoas")}>
+                    Pessoas
+                </button>
+
+                <button onClick={() => setPagina("transacoes")}>
+                    Transações
+                </button>
+
+                <button onClick={() => setPagina("relatorio")}>
+                    Relatório
+                </button>
+            </nav>
+
+
+            <hr />
+
+
+            {pagina === "pessoas" && <Pessoas />}
+
+            {pagina === "transacoes" && <Transacoes />}
+
+            {pagina === "relatorio" && <Relatorio />}
+        </>
+    );
 }
+
 
 export default App;
